ad11d5bdbef5ccd103fc0590e5636ada7d0a4c69
MMRREE <34013547+MMRREE@users.noreply.github.com>
2017-11-27 18:37:04 +0000

    Update go.c

--- go.c
+++ go.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+//#inlcude <ctype.h>
+
 /*
 int checkmid(int * position);
 int checktop(int * position);
@@ -8,6 +10,11 @@ int checkleft(int * position);
 int checklright(int * position);
 int checkbottom(int * position);
 */
+
+enum stone{clear, white, black};
+
+void black_input(int board[9][9]);
+
 void delay(int milli_seconds)
 {
         // Stroing start time
@@ -25,32 +32,11 @@ int main()
     int a = 0;
     int b = 0;
 
-
-    while(loop <10){
-        for(a = 0; a<9;a++){
-            for(b = 0; b<9; b++){
-                if (a==0 && b == 0){
-                    realboard[a][b] = 1;
-                    realboard[8][8] = 0;
-
-                }else if(a!=0 && b==0){
-                    realboard[a][b] = 1;
-                    realboard[a-1][8] = 0;
-                }
-                else{
-                    realboard[a][b] = 1;
-                    realboard[a][b-1] = 0;
-
-                }
-                delay(250);
-
-                printboard(9,9,realboard);
-
-                //delay(1000);
-            }
-        }
-        loop++;
-    }
+	while(1){
+		black_input(realboard);
+	printboard(9,9,realboard);
+	delay(100);
+	}
     return 0;
 }
 
@@ -61,13 +47,41 @@ void printboard(int size_x, int size_y, int board[size_y][size_x]){
     int k = 0;
     for(j = 0; j<size_y; j++){
         for (k = 0; k<size_x; k++){
-            if(board[j][k]==0)printf("0 ");
-            if(board[j][k]==1)printf("w ");
-            if(board[j][k]==2)printf("b ");
+            if(board[j][k]==clear)printf("0 ");
+            if(board[j][k]==white)printf("w ");
+            if(board[j][k]==black)printf("b ");
 
         }
 
         printf("\n");
     }
-    return 0;
+}
+
+void black_input(int board[9][9]){
+	char cposx[3]={};
+	int iposx;
+	int posy;
+	printf("Insert coordinates for the position of placing the stone\n");
+	gets(cposx);
+	if(cposx[0]=='A'){
+		iposx=0;
+	} if (cposx[0]=='B'){
+		iposx=1;
+	} if (cposx[0]=='C'){
+		iposx=2;
+	} if (cposx[0]=='D'){
+		iposx=3;
+	} if (cposx[0]=='E'){
+		iposx=4;
+	} if (cposx[0]=='F'){
+		iposx=5;
+	} if (cposx[0]=='G'){
+		iposx=6;
+	} if (cposx[0]=='F'){
+		iposx=7;
+	} if (cposx[0]=='H'){
+		iposx=8;
+	}
+	posy=cposx[1];
+	board[iposx][posy]=black;
 }
